0962e459b4e0ea19e464eddb684304e11089a085
riyyan-optiverse <riyyanoptiverse@gmail.com>
2026-02-12 11:43:02 +0500

    fix: fixed width of div

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,16 +7,16 @@ export const Home = () => {
     <div className="bg-sky-50 w-11/12 max-w-6xl mx-auto  shadow-xl ">
       <MainCard />
       <div className="flex justify-center items-center">
-        <div className="w-full h-10 max-w-3xl bg-yellow-400  pl-10 flex items-center">
+        <div className="w-[70%] h-10  bg-yellow-400  pl-10 flex items-center">
           <p className="text-lg font-semibold text-white">Pricing</p>
         </div>
-        <div className="w-96 h-10 bg-sky-100 "></div>
+        <div className="w-[30%] h-10 bg-sky-100 "></div>
       </div>
       <div className="mt-4 flex justify-end pr-10">
         <InputField />
       </div>
       <ProductCards />
-      <div className="px-10 pr-80">
+      <div className="px-10 ">
         <p className="text-sm font-normal text-gray-600 ">
           "All our mineral water bottles are carefully cleaned and tested to
           ensure the highest quality. We offer flexible delivery plans, simple
