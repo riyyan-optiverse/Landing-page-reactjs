08afed822848e02de9199e994992106e3a89b55d
riyyan-optiverse <riyyanoptiverse@gmail.com>
2026-02-18 10:10:08 +0500

    modified the footer

--- src/components/footer/footerContent.jsx
+++ src/components/footer/footerContent.jsx
@@ -1,42 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const footerLinks = [
+  {
+    text: "Home",
+    href: "home",
+  },
+  {
+    text: "Property-listing",
+    href: "property-listing",
+  },
+  {
+    text: "Post-Propert",
+    href: "post-property",
+  },
+  {
+    text: "Blog",
+    href: "blog",
+  },
+  {
+    text: "Contact us",
+    href: "contact-us",
+  },
+];
 const Footer = () => {
+  const [active, setactive] = useState("home");
   return (
     <div className="w-full bg-orange-500 mt-5">
       <div className="px-6 md:px-16 pt-7 flex flex-col items-center md:flex-row gap-10 md:gap-44">
         <div className="flex flex-col gap-6 ">
           <p className="text-2xl font-medium text-white">Quick Links</p>
           <div className="flex flex-col gap-1 items-center md:items-start">
-            <Link
-              to="/home"
-              className="text-lg text-white cursor-pointer hover:text-blue-500"
-            >
-              Home
-            </Link>
-            <Link
-              to="/property-listing"
-              className="text-lg text-white cursor-pointer hover:text-blue-500"
-            >
-              Property listing
-            </Link>
-            <Link
-              to="/post-property"
-              className="text-lg text-white cursor-pointer hover:text-blue-500"
-            >
-              Post property
-            </Link>
-            <Link
-              to="/blog"
-              className="text-lg text-white cursor-pointer hover:text-blue-500"
-            >
-              Blogs
-            </Link>
-            <Link
-              to="/contact-us"
-              className="text-lg text-white  cursor-pointer hover:text-blue-500"
-            >
-              Contact us
-            </Link>
+            {footerLinks.map((link, index) => {
+              return (
+                <Link
+                  key={index}
+                  to={`/${link.href}`}
+                  onClick={() => setactive(link.href)}
+                  className={`text-lg  cursor-pointer 
+                ${
+                  active === link.href
+                    ? "text-blue-600 border-b-2 border-blue-600"
+                    : "text-white hover:text-blue-500"
+                }
+                `}
+                >
+                  {link.text}
+                </Link>
+              );
+            })}
           </div>
         </div>
         <div className="flex flex-col gap-6 items-center md:items-start">
